9b978aba6a0306f50a673b9cd03ef469623d1383
MinisX <daniel.dedoukh@gmail.com>
2021-11-09 22:44:05 +0100

    only imported package

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -2,7 +2,7 @@ import 'package:crew_brew/screens/authenticate/authenticate.dart';
 import 'package:crew_brew/screens/home/home.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:crew_brew/models/AppUser.dart';
+import 'package:crew_brew/models/user/AppUser.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({Key? key}) : super(key: key);
