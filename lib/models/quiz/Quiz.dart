4b63bbf538f2bfbcdf4fb663a38f982826f2e1ba
Moe-Ara <hulk78901@gmail.com>
2021-11-25 22:54:18 +0100

    nothing important

--- lib/models/quiz/Quiz.dart
+++ lib/models/quiz/Quiz.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: file_names
+
 import 'package:crew_brew/models/quiz/question.dart';
 
 // ! Information about the class:
@@ -20,15 +22,16 @@ class Quiz {
   /*List<Question> listOfQuestions;
   List<String> tags;*/
 
-  Quiz(
-      {required this.quizCategory,
-      required this.quizTitle,
-      required this.quizOwner,
-      required this.quizOwnerUID,
-      required this.quizDescription,
-      required this.quizIsShared,
-      /*required this.listOfQuestions,
-      required this.tags*/});
+  Quiz({
+    required this.quizCategory,
+    required this.quizTitle,
+    required this.quizOwner,
+    required this.quizOwnerUID,
+    required this.quizDescription,
+    required this.quizIsShared,
+    /*required this.listOfQuestions,
+      required this.tags*/
+  });
 
 /*  int getNumberOfQuestions(int id) {
     return listOfQuestions.length;
@@ -37,5 +40,4 @@ class Quiz {
   String getTitleOfQuiz() {
     return quizTitle;
   }
-
 }
