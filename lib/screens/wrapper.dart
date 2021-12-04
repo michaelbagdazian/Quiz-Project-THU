0364d0a95ee395d07ab017f8060544c5e364da49
MinisX <daniel.dedoukh@gmail.com>
2021-12-04 14:59:48 +0100

    fixed wrapper mistake

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,7 +1,5 @@
 import 'package:crew_brew/screens/authenticate/WelcomingScreen.dart';
 import 'package:crew_brew/screens/home/home.dart';
-import 'package:crew_brew/screens/quizes/AddNewQuizzUI.dart';
-import 'package:crew_brew/screens/quizes/addQuestionsUI.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:crew_brew/models/user/AppUser.dart';
@@ -28,7 +26,7 @@ class Wrapper extends StatelessWidget {
 
     // ! If user is not logged in, return Welcome() screen. Otherwise Home() screen
     if (user == null) {
-      return AddQuestionsUI();
+      return WelcominScreen();
     } else {
       return Home();
     }
