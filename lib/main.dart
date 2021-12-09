1b9062f3e5723f8232a23d07ea2c49dfb028ed6b
Chrestschen <71799785+Chrestschen@users.noreply.github.com>
2021-12-09 06:00:50 +0100

    fine tunning

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,22 @@
-import 'package:crew_brew/models/quiz/Quiz.dart';
 import 'package:crew_brew/screens/home/home.dart';
-import 'package:crew_brew/screens/quizes/myQuizes.dart';
-import 'package:crew_brew/screens/quizes/sharedQuizes.dart';
-import 'package:crew_brew/screens/quizes/addQuestionsUI.dart';
+import 'package:crew_brew/screens/quizzes/add_new_quiz_ui.dart';
+import 'package:crew_brew/screens/quizzes/my_quizzes.dart';
+import 'package:crew_brew/screens/quizzes/quiz_wrapper.dart';
+import 'package:crew_brew/screens/quizzes/shared_quizzes.dart';
+import 'package:crew_brew/screens/quizzes/add_questions_ui.dart';
 import 'package:crew_brew/screens/userProfile/userProfile.dart';
 import 'package:crew_brew/screens/wrapper.dart';
-import 'package:crew_brew/services/AddQuestion.dart';
 import 'package:crew_brew/services/auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:crew_brew/models/user/AppUser.dart';
 import 'package:crew_brew/screens/authenticate/register.dart';
 import 'package:crew_brew/screens/authenticate/sign_in.dart';
 import 'package:crew_brew/screens/authenticate/WelcomingScreen.dart';
+import 'package:crew_brew/shared/colors.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 
+import 'navigationBar/nav_container.dart';
 // ! Information about the class:
 // ~ Main class
 // ! Use of the class:
@@ -46,15 +48,21 @@ class MyApp extends StatelessWidget {
         // ! routes
         // ~ Here we define all routes of our apps. They are mainly used in navigationBar/navBar.dart -> selectedItem()
         routes: {
-          '/home': (context) => Home(),
-          '/userProfile': (context) => const userProfile(),
-          '/sharedQuizes': (context) => SharedQuizes(),
-          '/myQuizes': (context) => MyQuizes(),
-          '/welcome': (context) => WelcominScreen(),
-          '/register': (context) => SignUp(),
-          '/signin': (context) => LogIn(),
+          '/home': (context) => const NavContainer(page: Home()),
+          '/userProfile': (context) => const NavContainer(page: userProfile()),
+          '/sharedQuizes': (context) => NavContainer(page: SharedQuizes()),
+          '/myQuizes': (context) => const NavContainer(page: MyQuizes()),
+          '/welcome': (context) => const NavContainer(page: WelcominScreen()),
+          '/register': (context) => const SignUp(),
+          '/signin': (context) => const LogIn(),
+          '/quizWrapper': (context) => const QuizWrapper(),
           '/AddQuestionsUI': (context) => AddQuestionsUI(),
+          '/AddNewQuizzUI': (context) => const AddNewQuizzUI(),
         },
+        // TODO: Decide on a unified theme and copy it here
+        theme: ThemeData.light().copyWith(
+          backgroundColor: background,
+        ),
       ),
     );
   }
