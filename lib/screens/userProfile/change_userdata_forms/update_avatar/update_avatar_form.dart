18e1ca205966caf51e7bb9639b849db77b70871f
Moe-Ara <hulk78901@gmail.com>
2022-01-05 20:46:02 +0100

    added an unfunctional option to upload your own avatar

--- lib/screens/userProfile/change_userdata_forms/update_avatar/update_avatar_form.dart
+++ lib/screens/userProfile/change_userdata_forms/update_avatar/update_avatar_form.dart
@@ -1,6 +1,7 @@
 import 'dart:collection';
 
 import 'package:crew_brew/screens/userProfile/change_userdata_forms/update_avatar/avatar_tile.dart';
+import 'package:crew_brew/services/UploadImageToFireStorage.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../../../models/user/AppUser.dart';
@@ -25,6 +26,8 @@ class _AvatarFormState extends State<AvatarForm> {
   Widget build(BuildContext context) {
     final userData = Provider.of<UserData?>(context);
     final user = Provider.of<AppUser?>(context);
+    ValueNotifier<String> url = ValueNotifier<String>(
+        'https://firebasestorage.googleapis.com/v0/b/daniel-brew-crew-20887.appspot.com/o/Default_Avatars%2Fceratops%2Fceratops2.png?alt=media&token=393336fa-a445-4f80-a241-084b0ebb68bd');
 
     if (userData != null && user != null) {
       return Container(
@@ -42,26 +45,70 @@ class _AvatarFormState extends State<AvatarForm> {
                   // ! itemBuilder:
                   // ~ itemBuilder is the function in itself which is going to return some kind of template or a widget tree for each item inside the list
                   itemBuilder: (context, index) {
-                    return AvatarTile(avatar: avatars.entries.elementAt(index), setAvatarPath: setAvatarPath);
+                    return AvatarTile(
+                        avatar: avatars.entries.elementAt(index),
+                        setAvatarPath: setAvatarPath);
                   },
                 ),
               ),
-              RaisedButton(
-                  color: buttons,
-                  child: Text(
-                    'Update',
-                    style: TextStyle(color: Colors.white),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 4.0),
+                    child: RaisedButton(
+                        color: buttons,
+                        child: Text(
+                          'Update',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            await DatabaseService(uid: user.uid).updateUserData(
+                                userData.username,
+                                userData.email,
+                                _avatarPath.isEmpty
+                                    ? userData.avatar
+                                    : _avatarPath,
+                                userData.points);
+                            Navigator.pop(context);
+                          }
+                        }),
                   ),
-                  onPressed: () async {
-                    if (_formKey.currentState!.validate()) {
-                      await DatabaseService(uid: user.uid).updateUserData(
-                          userData.username,
-                          userData.email,
-                          _avatarPath.isEmpty ? userData.avatar : _avatarPath,
-                          userData.points);
-                      Navigator.pop(context);
-                    }
-                  }),
+                  //* The following button is in case we want to let the user upload their own avatar-
+                  // Padding(
+                  //   padding: const EdgeInsets.only(left: 4.0),
+                  //   child: RaisedButton(
+                  //       color: buttons,
+                  //       child: Text(
+                  //         'Browse',
+                  //         style: TextStyle(color: Colors.white),
+                  //       ),
+                  //       onPressed: () async {
+                  //         UploadFileToFireStorage UA =
+                  //             new UploadFileToFireStorage();
+                  //         try {
+                  //           await UA.uploadPicture(path: 'Avatar');
+                  //           await UA.deleteFileFromFirebaseByUrl(
+                  //               userData.avatar,
+                  //               path: 'Avatar',
+                  //               userData: userData);
+                  //         } catch (e) {
+                  //           return;
+                  //         }
+                  //         url.value = UA.url;
+                  //         String uid = userData.uid;
+                  //         DatabaseService(uid: uid).updateUserData(
+                  //             userData.username,
+                  //             userData.email,
+                  //             UA.url,
+                  //             userData.points);
+                  //         Navigator.pop(context);
+                  //       }),
+                  //),
+                ],
+              ),
             ],
           ),
         ),
@@ -71,7 +118,7 @@ class _AvatarFormState extends State<AvatarForm> {
     }
   }
 
-  void setAvatarPath(String avatarPath){
+  void setAvatarPath(String avatarPath) {
     _avatarPath = avatarPath;
   }
 }
@@ -93,7 +140,12 @@ List<String> _createAvatars(String avatarName) {
   List<String> avatarsList = [];
 
   for (int i = 1; i <= 5; i++) {
-    avatarsList.add("assets/avatars/"+avatarName+"/"+avatarName+i.toString()+".png");
+    avatarsList.add("assets/avatars/" +
+        avatarName +
+        "/" +
+        avatarName +
+        i.toString() +
+        ".png");
   }
 
   return avatarsList;
