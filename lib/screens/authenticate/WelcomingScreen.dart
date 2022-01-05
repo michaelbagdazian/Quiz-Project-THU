c27c65cff3408ab31795145537bfcc80ec67a8c4
MinisX <daniel.dedoukh@gmail.com>
2022-01-05 21:20:11 +0100

    merged with Mohamad

--- lib/screens/authenticate/WelcomingScreen.dart
+++ lib/screens/authenticate/WelcomingScreen.dart
@@ -58,18 +58,20 @@ class _WelcominScreenState extends State<WelcominScreen> {
   @override
   Widget build(BuildContext context) {
     /// ~ size of the screen
-    Size size = MediaQuery.of(context).size;
+    Size size = MediaQuery
+        .of(context)
+        .size;
 
     /// ~ sizes per widget
-    double welcomeTextSize = size.height * (8/100);
-    double eduLogoHeight = size.height * (13/100.0);
-    double verticalPadding = size.height * (1/100);
-    double horizontalPadding = size.width * (20/100);
-    double sizedBoxHeight = size.height * (3/100);
-    double inputTextHintSize = size.height * (10/100);
-    double inputBarWidth = size.width * (60/100);
-    double buttonWidth = size.width *(8/100);
-    double buttonHeight = size.height * (20/100);
+    double welcomeTextSize = size.height * (8 / 100);
+    double eduLogoHeight = size.height * (13 / 100.0);
+    double verticalPadding = size.height * (1 / 100);
+    double horizontalPadding = size.width * (20 / 100);
+    double sizedBoxHeight = size.height * (3 / 100);
+    double inputTextHintSize = size.height * (10 / 100);
+    double inputBarWidth = size.width * (60 / 100);
+    double buttonWidth = size.width * (8 / 100);
+    double buttonHeight = size.height * (20 / 100);
 
     /*if(size.height > 1000 || size.width > 1000){
       inputBarWidth = size.width * (30/100);
@@ -89,35 +91,38 @@ class _WelcominScreenState extends State<WelcominScreen> {
               fit: BoxFit.cover),
         ),
         child:
-            //*children inside screen
-            Padding(
-          padding: EdgeInsets.all(verticalPadding),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              //*Text
-              _customText.customText("Welcome to\n The Quizzler", welcomeTextSize,
-                  backgroundColor: welcomeh),
-              //*Logo/pic on welcoming screen
-              eduLogo(eduLogoHeight),
-              SizedBox(
-                height: sizedBoxHeight,
-              ),
-              //*create space between the widget and the sides of the screen
-              CustomTextField().customTextField(
-                _displayNameController,
-                'Display Name',
-                inputBarWidth,
-                TextInputType.text,
-              ),
-              SizedBox(height: sizedBoxHeight),
-              //* Button
-              joinButton(context, buttonWidth, buttonHeight),
-              SizedBox(height: sizedBoxHeight),
-              //*just a widget with the buttons for login or sign up
-              //* frontend can improve the quality of this widget by using the custom Widgets; only if they want to
-              signInOrUp(context, buttonWidth, sizedBoxHeight),
-            ],
+        //*children inside screen
+        SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(verticalPadding),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //*Text
+                _customText.customText(
+                    "Welcome to\n The Quizzler", welcomeTextSize,
+                    backgroundColor: welcomeh),
+                //*Logo/pic on welcoming screen
+                eduLogo(eduLogoHeight),
+                SizedBox(
+                  height: sizedBoxHeight,
+                ),
+                //*create space between the widget and the sides of the screen
+                CustomTextField().customTextField(
+                  _displayNameController,
+                  'Display Name',
+                  inputBarWidth,
+                  TextInputType.text,
+                ),
+                SizedBox(height: sizedBoxHeight),
+                //* Button
+                joinButton(context, buttonWidth, buttonHeight),
+                SizedBox(height: sizedBoxHeight),
+                //*just a widget with the buttons for login or sign up
+                //* frontend can improve the quality of this widget by using the custom Widgets; only if they want to
+                signInOrUp(context, buttonWidth, sizedBoxHeight),
+              ],
+            ),
           ),
         ),
       ),
@@ -127,7 +132,7 @@ class _WelcominScreenState extends State<WelcominScreen> {
   Widget joinButton(BuildContext context, double width, double height) {
     return FloatingActionButton.extended(
       heroTag: "joinButton",
-      extendedPadding: EdgeInsets.all(width*0.95),
+      extendedPadding: EdgeInsets.all(width * 0.95),
       extendedIconLabelSpacing: 10,
       onPressed: () async {
         //get the entered display name
@@ -175,7 +180,7 @@ class _WelcominScreenState extends State<WelcominScreen> {
       mainAxisAlignment: MainAxisAlignment.end,
       mainAxisSize: MainAxisSize.min,
       children: [
-        CustomText().customText('--- or ---',30),
+        CustomText().customText('--- or ---', 30),
         SizedBox(
           height: sizedBoxHeight,
         ),
@@ -235,8 +240,7 @@ class _WelcominScreenState extends State<WelcominScreen> {
   //?these are to redirect user to a link
   _launchURLApp() async {
     const url = 'https://flutterdevs.com/';
-    if (await launch(url, forceSafariVC: true, forceWebView: true)) {
-    } else {
+    if (await launch(url, forceSafariVC: true, forceWebView: true)) {} else {
       throw 'Could not launch $url';
     }
   }
