144026aeec1b66ac95a478a96b5ce104484d9e4b
Moe-Ara <hulk78901@gmail.com>
2022-01-05 19:32:07 +0100

    made them scrollable to avoid overflow problems

--- lib/screens/userProfile/change_userdata_forms/update_username_form.dart
+++ lib/screens/userProfile/change_userdata_forms/update_username_form.dart
@@ -30,37 +30,40 @@ class _UsernameFormState extends State<UsernameForm> {
         height: 200.0,
         child: Form(
           key: _formKey,
-          child: Column(
-            children: <Widget>[
-              Text(
-                'Update username',
-                style: TextStyle(fontSize: 18.0, color: Colors.white),
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                initialValue: userData.username,
-                decoration: textInputDecoration,
-                validator: (val) => val!.isEmpty ? 'Please enter a name' : null,
-                onChanged: (val) => setState(() => _username = val),
-              ),
-              SizedBox(height: 10.0),
-              RaisedButton(
-                  color: buttons,
-                  child: Text(
-                    'Update',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  onPressed: () async {
-                    if (_formKey.currentState!.validate()) {
-                      await DatabaseService(uid: user.uid).updateUserData(
-                          _username.isEmpty ? userData.username : _username,
-                          userData.email,
-                          userData.avatar,
-                          userData.points);
-                      Navigator.pop(context);
-                    }
-                  }),
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Text(
+                  'Update username',
+                  style: TextStyle(fontSize: 18.0, color: Colors.white),
+                ),
+                SizedBox(height: 20.0),
+                TextFormField(
+                  initialValue: userData.username,
+                  decoration: textInputDecoration,
+                  validator: (val) =>
+                      val!.isEmpty ? 'Please enter a name' : null,
+                  onChanged: (val) => setState(() => _username = val),
+                ),
+                SizedBox(height: 10.0),
+                RaisedButton(
+                    color: buttons,
+                    child: Text(
+                      'Update',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        await DatabaseService(uid: user.uid).updateUserData(
+                            _username.isEmpty ? userData.username : _username,
+                            userData.email,
+                            userData.avatar,
+                            userData.points);
+                        Navigator.pop(context);
+                      }
+                    }),
+              ],
+            ),
           ),
         ),
       );
