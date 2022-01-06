beb70a911b0c544f92ad64a43dac2f399ac4b185
MinisX <daniel.dedoukh@gmail.com>
2022-01-06 18:12:38 +0100

    now points are added after you have played the quiz

--- lib/screens/result/result.dart
+++ lib/screens/result/result.dart
@@ -1,7 +1,13 @@
 import 'package:collection/collection.dart';
 import 'package:crew_brew/models/quiz/quiz_state.dart';
+import 'package:crew_brew/services/database.dart';
 import 'package:crew_brew/shared/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../models/user/AppUser.dart';
+import '../../models/user/UserData.dart';
+import '../../shared/loading.dart';
 
 class Result extends StatelessWidget {
   const Result({Key? key}) : super(key: key);
@@ -11,128 +17,173 @@ class Result extends StatelessWidget {
     final args = ModalRoute.of(context)!.settings.arguments as QuizState;
     ThemeData theme = Theme.of(context);
     final em = theme.textTheme.bodyText2?.fontSize ?? 16;
-    return Scaffold(
-      backgroundColor: background,
-      body: SafeArea(
-        child: Container(
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage('assets/images/bgtop.png'),
-                  fit: BoxFit.cover)),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(top: 2 * em),
-                child: SizedBox(
-                    width: 7 * em,
-                    height: 7 * em,
-                    child: CircularProgressIndicator(
-                      color: theme.primaryColor,
-                      strokeWidth: 10,
-                      value: args.stateVector.playerPoints[0].sum /
-                          args.quiz.listOfQuestions.length,
-                    )),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 2 * em, bottom: 2 * em),
-                child: Text(
-                  "You got ${args.stateVector.playerPoints[0].sum.toString()}/${args.quiz.listOfQuestions.length}",
-                  textAlign: TextAlign.center,
-                  style:
-                      theme.textTheme.headline5!.copyWith(color: Colors.white),
-                ),
-              ),
-              Expanded(
-                child: ListView(
-                  children: <Widget>[
-                    for (var i = 0, k = 0;
-                        i < args.quiz.listOfQuestions.length;
-                        i++) //
-                      Card(
-                        color: const Color.fromARGB(25, 0, 0, 0),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Padding(
-                          padding: EdgeInsets.all(1 * em),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Padding(
-                                padding: EdgeInsets.only(
-                                    top: 1.5 * em, bottom: 2 * em),
-                                child: Text(
-                                  args.quiz.listOfQuestions[i].questionText,
-                                  style: theme.textTheme.headline6!
-                                      .copyWith(color: Colors.white),
-                                ),
-                              ),
-                              Column(children: <Widget>[
-                                for (var j = 0;
-                                    j <
-                                        args.quiz.listOfQuestions[i].answers
-                                            .length;
-                                    j++, k++)
-                                  Container(
-                                    margin: EdgeInsets.all(0.5 * em),
-                                    width: double.infinity,
-                                    decoration: BoxDecoration(
-                                        borderRadius:
-                                        BorderRadius.circular(8),
-                                        border: args.stateVector.buttonsPressedSaved[0][k] ? Border.all(color: Colors.tealAccent.shade400, width: 4) : null,
-                                        color: args.quiz.listOfQuestions[i].answers[j].isCorrect
-                                            ? Colors.green
-                                            : Colors.red),
-                                    /*BoxDecoration(
-                                        borderRadius: BorderRadius.circular(8),
-                                        color: args.quiz.listOfQuestions[i]
-                                                .answers[j].isCorrect
-                                            ? Colors.green
-                                            : Colors.red),*/
-                                    child: Padding(
-                                      padding: EdgeInsets.all(1.5 * em),
-                                      child: Text(
-                                          args.quiz.listOfQuestions[i]
-                                                  .answers[j].answerText,
-                                              /*+
-                                              // Holger fix this shit!!
-                                              ((args.stateVector
-                                                      .buttonsPressedSaved[0][k])
-                                                  ? ' [Selected]'
-                                                  : ''),*/
-                                          style: theme.textTheme.bodyText1!
-                                              .copyWith(color: Colors.white)),
+
+    /// ! update user points in DB
+    final user = Provider.of<AppUser?>(context);
+
+    if (user != null) {
+      return StreamBuilder<UserData>(
+          stream: DatabaseService(uid: user.uid).userData,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              UserData? userData = snapshot.data;
+              String username = userData!.username;
+              String email = userData.email;
+              String avatar = userData.avatar;
+              int points = args.stateVector.playerPoints[0].sum;
+              DatabaseService(uid: user.uid)
+                  .updateUserData(username, email, avatar, points);
+
+              /// ! Finish updating user points in DB
+
+              return Scaffold(
+                backgroundColor: background,
+                body: SafeArea(
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/bgtop.png'),
+                            fit: BoxFit.cover)),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(top: 2 * em),
+                          child: SizedBox(
+                              width: 7 * em,
+                              height: 7 * em,
+                              child: CircularProgressIndicator(
+                                color: theme.primaryColor,
+                                strokeWidth: 10,
+                                value: args.stateVector.playerPoints[0].sum /
+                                    args.quiz.listOfQuestions.length,
+                              )),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 2 * em, bottom: 2 * em),
+                          child: Text(
+                            "You got ${args.stateVector.playerPoints[0].sum.toString()}/${args.quiz.listOfQuestions.length}",
+                            textAlign: TextAlign.center,
+                            style: theme.textTheme.headline5!
+                                .copyWith(color: Colors.white),
+                          ),
+                        ),
+                        Expanded(
+                          child: ListView(
+                            children: <Widget>[
+                              for (var i = 0, k = 0;
+                                  i < args.quiz.listOfQuestions.length;
+                                  i++) //
+                                Card(
+                                  color: const Color.fromARGB(25, 0, 0, 0),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10)),
+                                  child: Padding(
+                                    padding: EdgeInsets.all(1 * em),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Padding(
+                                          padding: EdgeInsets.only(
+                                              top: 1.5 * em, bottom: 2 * em),
+                                          child: Text(
+                                            args.quiz.listOfQuestions[i]
+                                                .questionText,
+                                            style: theme.textTheme.headline6!
+                                                .copyWith(color: Colors.white),
+                                          ),
+                                        ),
+                                        Column(children: <Widget>[
+                                          for (var j = 0;
+                                              j <
+                                                  args.quiz.listOfQuestions[i]
+                                                      .answers.length;
+                                              j++, k++)
+                                            Container(
+                                              margin: EdgeInsets.all(0.5 * em),
+                                              width: double.infinity,
+                                              decoration: BoxDecoration(
+                                                  borderRadius:
+                                                      BorderRadius.circular(8),
+                                                  border: args.stateVector
+                                                              .buttonsPressedSaved[
+                                                          0][k]
+                                                      ? Border.all(
+                                                          color: Colors
+                                                              .tealAccent
+                                                              .shade400,
+                                                          width: 4)
+                                                      : null,
+                                                  color: args
+                                                          .quiz
+                                                          .listOfQuestions[i]
+                                                          .answers[j]
+                                                          .isCorrect
+                                                      ? Colors.green
+                                                      : Colors.red),
+                                              /*BoxDecoration(
+                                            borderRadius: BorderRadius.circular(8),
+                                            color: args.quiz.listOfQuestions[i]
+                                                    .answers[j].isCorrect
+                                                ? Colors.green
+                                                : Colors.red),*/
+                                              child: Padding(
+                                                padding:
+                                                    EdgeInsets.all(1.5 * em),
+                                                child: Text(
+                                                    args.quiz.listOfQuestions[i]
+                                                        .answers[j].answerText,
+                                                    /*+
+                                                  // Holger fix this shit!!
+                                                  ((args.stateVector
+                                                          .buttonsPressedSaved[0][k])
+                                                      ? ' [Selected]'
+                                                      : ''),*/
+                                                    style: theme
+                                                        .textTheme.bodyText1!
+                                                        .copyWith(
+                                                            color:
+                                                                Colors.white)),
+                                              ),
+                                            )
+                                        ])
+                                      ],
                                     ),
-                                  )
-                              ])
+                                  ),
+                                )
                             ],
                           ),
                         ),
-                      )
-                  ],
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 1 * em),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: ElevatedButton(
-                        onPressed: () => Navigator.pushReplacementNamed(
-                            context, '/sharedQuizes'),
-                        child: const Text("back"),
-                        style: ElevatedButton.styleFrom(
-                            primary: theme.primaryColor,
-                            padding: const EdgeInsets.all(20)),
-                      ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 1 * em),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: ElevatedButton(
+                                  onPressed: () =>
+                                      Navigator.pushReplacementNamed(
+                                          context, '/sharedQuizes'),
+                                  child: const Text("back"),
+                                  style: ElevatedButton.styleFrom(
+                                      primary: theme.primaryColor,
+                                      padding: const EdgeInsets.all(20)),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+              );
+            } else {
+              return Loading();
+            }
+          });
+    } else {
+      return Loading();
+    }
   }
 }
