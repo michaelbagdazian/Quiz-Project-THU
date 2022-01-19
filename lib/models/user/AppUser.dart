4dee113c97d573ad1d6152e4e1b6bda3c4cf7322
Tiwugyit <92168015+Tiwugyit@users.noreply.github.com>
2022-01-19 13:05:51 +0100

    Update AppUser.dart

--- lib/models/user/AppUser.dart
+++ lib/models/user/AppUser.dart
@@ -1,13 +1,13 @@
-// ! Infomartion about the class:
-// ~ This class represents the User instance from Firestore Database, but we define our own for convenience.
-// ! Use of class:
-// ~ In services/auth.dart we return the instance of this class in method _userFromFirebaseUser and then make use of it in the defined Stream<AppUser>.
+/// ! Infomartion about the class:
+/// ~ This class represents the User instance from Firestore Database, but we define our own for convenience.
+/// ! Use of class:
+/// ~ In services/auth.dart we return the instance of this class in method _userFromFirebaseUser and then make use of it in the defined Stream<AppUser>.
 
 // ! TODOS:
 // all done
 
 class AppUser {
-  // ~ UID is all we need to understand if user is logged-in or not
+  /// ~ UID is all we need to understand if user is logged-in or not
   final String uid;
 
   AppUser({required this.uid});
